a1d7a957d46693c79e2cab6889015c8a11978ad1
neogeoss <neogeo-s@hanmail.net>
2019-05-12 20:52:51 +0900

    Constructor injection added and implemented

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,10 +1,17 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
+	private FortuneService fortuneService;
+	
+	@Autowired
+	public TennisCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
 	@Override
 	public String getDailyWorkout() {
 		// TODO Auto-generated method stub
@@ -14,7 +21,7 @@ public class TennisCoach implements Coach {
 	@Override
 	public String getDailyFortune() {
 		// TODO Auto-generated method stub
-		return null;
+		return fortuneService.getFortune();
 	}
 
 }
