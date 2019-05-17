82d07322772caf9a6296a4690b35b630b6f10530
neogeoss <neogeo-s@hanmail.net>
2019-05-18 04:10:55 +0900

    Initial and destroy methods example

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,12 +1,15 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
+
 public class TennisCoach implements Coach {
 
 	@Autowired
@@ -18,9 +21,22 @@ public class TennisCoach implements Coach {
 //		fortuneService = theFortuneService;
 //	}
 	//Define a default constructor
+	
 	public TennisCoach() {
 		System.out.println(">> inside default constructor");
 	}
+	
+	// define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyStartupStuff");
+	}
+	
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyCleanupStuff");
+	}
+	//define my destroy method
 	
 	//define a setter method
 //	@Autowired
