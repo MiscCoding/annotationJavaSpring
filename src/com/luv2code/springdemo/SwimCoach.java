f616770646217279cfe472e5fa9033304158c502
neogeoss <neogeo-s@hanmail.net>
2019-05-21 23:11:21 +0900

    Value injection from files

--- src/com/luv2code/springdemo/SwimCoach.java
+++ src/com/luv2code/springdemo/SwimCoach.java
@@ -1,8 +1,29 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
+
 public class SwimCoach implements Coach {
 	
 	private FortuneService fortuneService;
+	
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
+	
+	public String getEmail() {
+		return email;
+	}
+
+	
+
+	public String getTeam() {
+		return team;
+	}
+	
+	
 	
 	public SwimCoach(FortuneService theFortuneService) {
 		fortuneService = theFortuneService;
@@ -19,5 +40,11 @@ public class SwimCoach implements Coach {
 		// TODO Auto-generated method stub
 		return fortuneService.getFortune();
 	}
+
+	
+
+	
+	
+	
 
 }
--- src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
+++ src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
@@ -11,13 +11,16 @@ public class SwimJavaConfigDemoApp {
 		AnnotationConfigApplicationContext context =
 				new AnnotationConfigApplicationContext(SportConfig.class);
 		//get the bean from spring container
-		Coach theCoach = context.getBean("swimCoach", Coach.class);
+		SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
 		
 		//call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		
 		//call method to get the daily fortune
 		System.out.println(theCoach.getDailyFortune());
+		
+		System.out.println(theCoach.getEmail());
+		System.out.println(theCoach.getTeam());
 		
 		//hahah
 		//close the content
